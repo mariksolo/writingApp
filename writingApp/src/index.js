d76af49b3365c4e5bd0d1b4fbb1c5db66afdd05a
Mark Solomonik <marik.solomonik@gmail.com>
2019-11-17 12:32:42 -0800

    Created working POST form

--- writingApp/src/index.js
+++ writingApp/src/index.js
@@ -6,7 +6,11 @@ require("./db/db");
 
 const app = express();
 const port = process.env.PORT;
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 app.use(userRouter)
 
